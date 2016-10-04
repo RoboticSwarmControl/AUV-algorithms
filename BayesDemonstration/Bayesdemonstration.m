941d96200c60ec8a15d43b9a8490f7da30b3b786
Aaron Becker <aabecker@gmail.com>
2016-10-04 10:50:23 -0500

    tab-aligned the code

--- BayesDemonstration/Bayesdemonstration.m
+++ BayesDemonstration/Bayesdemonstration.m
@@ -1,31 +1,20 @@
 %This code is use for demonstrating Bayes rule can implement to underwater
 %localization
 %
-%
-%
-% 
 %The algorithm is based on range based schemes localization
 %
-%
-% Outline:   
+% Outline:
 %    set a fix sensor position
-%    set five different position of boat 40 away from sensor 
+%    set five different position of boat 40 away from sensor
 %    each position measure distance bwt sensor
 %    implement bayes rule
 %    draw 2D image
 %    draw histogram
 %
+% result:
 %
-% result: 
-%    
+% Subfunctions:
 %
-%
-%
-% 
-% Subfunctions: 
-% 
-%
-
 % Author: Haoran Zhao, Master, Electrical Engineering
 % University of Houston
 % email address: zhaohaorandl@gmail.com
@@ -35,7 +24,8 @@
 clear
 clc
 digits(4);
-%%------------ main variable -----------
+format compact
+%%------------ main variables -----------
 sensor_position = [0,0];
 boat_position=zeros(1,2);
 xrange=-50:50;
@@ -59,8 +49,8 @@ for i=1:step
     boat_position = [sbdist*cos(theta),sbdist*sin(theta)];
     dist_error = sbdist+randn(1,1);
     for i1=1:r
-         dist_cell(i1,1)=dist(xy(i1,:),boat_position);
-         probs(i1)=updtprob(dist_cell(i1),dist_error,sd);
+        dist_cell(i1,1)=dist(xy(i1,:),boat_position);
+        probs(i1)=updtprob(dist_cell(i1),dist_error,sd);
     end
     
     for i2=1:r
@@ -69,9 +59,9 @@ for i=1:step
     normalizerb=1/sum(belief{i}(:));
     n=belief{i}.*normalizerb;
     for i4=1:i
-    m=m.*belief{i};
+        m=m.*belief{i};
     end
-%     m=m.*belief_init.^(i-1);
+    %     m=m.*belief_init.^(i-1);
     normalizer = 1/sum(m(:));
     m=m.*normalizer;
     b_var=var(m(:));
@@ -79,34 +69,34 @@ for i=1:step
     idx=find(fidx);
     [r1,~]=size(idx);
     for i3=1:r1
-       pts_in{i}(i3,:) =xy(idx(i3,1),:) ;
+        pts_in{i}(i3,:) =xy(idx(i3,1),:) ;
     end
     %%----------- figure(1) ---------------
-figure(1)
-hPpts = plot(pts_in{i}(:,1),pts_in{i}(:,2),'r.');
-hold on
-axis equal
-grid on
-hSpts = plot(sensor_position(1,1),sensor_position(1,2),'b+');
-hBpts = plot(boat_position(1,1),boat_position(1,2),'k+');
-axis([-50 50 -50 50]);
-legend('estimate position','sensor position','boat position');
-title(['Histogram after ',num2str(i),'  distance measurements'])
-
-figure(2)
-redrawWorld(m);
-hold on
-redrawWorld(n);
-title({['Histogram after ',num2str(i),'  distance measurements'],['Histogram variance = ', num2str(b_var)]})
-
-figure(3)
-redrawWorlds(m);
-hold on
-redrawWorlds(n);
-
-title({['Histogram after ',num2str(i),'  distance measurements'],['Histogram variance = ', num2str(b_var)]})
-
-
+    figure(1)
+    hPpts = plot(pts_in{i}(:,1),pts_in{i}(:,2),'r.');
+    hold on
+    axis equal
+    grid on
+    hSpts = plot(sensor_position(1,1),sensor_position(1,2),'b+');
+    hBpts = plot(boat_position(1,1),boat_position(1,2),'k+');
+    axis([-50 50 -50 50]);
+    legend('estimate position','sensor position','boat position');
+    title(['Histogram after ',num2str(i),'  distance measurements'])
+    
+    figure(2)
+    redrawWorld(m);
+    hold on
+    redrawWorld(n);
+    title({['Histogram after ',num2str(i),'  distance measurements'],['Histogram variance = ', num2str(b_var)]})
+    
+    figure(3)
+    redrawWorlds(m);
+    hold on
+    redrawWorlds(n);
+    
+    title({['Histogram after ',num2str(i),'  distance measurements'],['Histogram variance = ', num2str(b_var)]})
+    
+    
 end
 
 [r_est,c_est]=find(m==max(m(:)));
